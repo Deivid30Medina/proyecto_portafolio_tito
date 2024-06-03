538a6992a3549c1d95598fdfec672718b461d5eb
Deivid30Medina <deivis32sebas02@gmail.com>
2024-06-02 22:43:10 -0500

    Feat: Adding home

---
+++ scripts/components/sections.js
@@ -0,0 +1,31 @@
+const sections = [
+    "./pages/section-video.html"
+    ,"./pages/section-publicaciones.html"
+    ,"./pages/section-ilustraciones.html"
+    ,"./pages/section-sobremiVideos.html"
+    ,"./pages/section-sobremiInformacion.html"
+];
+
+function loadSection(section){
+    return fetch(section)
+        .then(response => response.text())
+        .catch(error => console.log("Erro al cargar la seccion: " + section));
+}
+
+async function loadAllSections(){
+    const main = document.getElementById("idMain");
+    for(let section of sections){
+        const sectionConten = await loadSection(section);
+        main.innerHTML += sectionConten;
+    }
+}
+
+window.onload = () => {
+    loadAllSections();
+};
+
+
+
+
+
+
