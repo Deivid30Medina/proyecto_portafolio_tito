48a223584f40ccd6ecdbca024a5cd3667d7b4035
Deivid30Medina <deivis32sebas02@gmail.com>
2024-06-16 12:37:29 -0500

    fix(publicaciones): slider

--- scripts/components/publicactions/slider.js
+++ scripts/components/publicactions/slider.js
@@ -1,82 +1,14 @@
-// // slider.js
-
-// /**
-//  * Función para inicializar el slider.
-//  */
-// export function initSlider() {
-//     // Selecciona el contenedor del slider, el botón previo y el botón siguiente
-//     const scrollContainer = document.querySelector(".sliderGallery");
-//     const prevBtn = document.getElementById("idBtnPrev");
-//     const nextBtn = document.getElementById("idBtnNex");
-
-//     // Verifica que todos los elementos necesarios existen
-//     if (scrollContainer && prevBtn && nextBtn) {
-//         const containerWidth = scrollContainer.offsetWidth; // Obtiene el ancho del contenedor del slider
-
-//         /**
-//          * Función para chequear y ajustar la posición del slider si es necesario.
-//          */
-//         const checkPosition = () => {
-//             // Si el scroll ha llegado al final, vuelve al inicio suavemente
-//             if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - containerWidth) {
-//                 scrollContainer.scrollTo({ left: 0, behavior: 'smooth' });
-//             } 
-//             // Si el scroll ha llegado al inicio, vuelve al final suavemente
-//             else if (scrollContainer.scrollLeft <= 0) {
-//                 scrollContainer.scrollTo({ left: scrollContainer.scrollWidth - containerWidth, behavior: 'smooth' });
-//             }
-//         };
-
-//         /**
-//          * Evento de desplazamiento con la rueda del mouse.
-//          * @param {Event} event - El evento de desplazamiento.
-//          */
-//         const handleWheelScroll = (event) => {
-//             event.preventDefault(); // Previene el comportamiento por defecto del scroll
-//             scrollContainer.scrollBy({ left: event.deltaY, behavior: 'smooth' }); // Desplaza el contenido suavemente
-//             checkPosition(); // Chequea y ajusta la posición si es necesario
-//         };
-
-//         /**
-//          * Evento para el botón siguiente.
-//          */
-//         const handleNextClick = () => {
-//             scrollContainer.scrollBy({ left: containerWidth / 2, behavior: 'smooth' }); // Desplaza el contenido a la derecha suavemente
-//             setTimeout(checkPosition, 300); // Chequea y ajusta la posición después de un tiempo
-//         };
-
-//         /**
-//          * Evento para el botón anterior.
-//          */
-//         const handlePrevClick = () => {
-//             scrollContainer.scrollBy({ left: -containerWidth / 2, behavior: 'smooth' }); // Desplaza el contenido a la izquierda suavemente
-//             setTimeout(checkPosition, 300); // Chequea y ajusta la posición después de un tiempo
-//         };
-
-//         // Inicializa la posición del scroll al inicio
-//         scrollContainer.scrollLeft = 0;
-
-//         // Asigna los eventos correspondientes
-//         scrollContainer.addEventListener("wheel", handleWheelScroll);
-//         nextBtn.addEventListener("click", handleNextClick);
-//         prevBtn.addEventListener("click", handlePrevClick);
-//     } else {
-//         console.error("No se encontraron los elementos del slider."); // Muestra un error si los elementos no fueron encontrados
-//     }
-// }
-
 // slider.js
 
-
-// slider.js
-
-/**
- * Función para inicializar el slider.
- */
 export function initSlider() {
     const scrollContainer = document.querySelector(".sliderGallery");
     const prevBtn = document.getElementById("idBtnPrev");
     const nextBtn = document.getElementById("idBtnNex");
+    const indicatorsContainer = document.querySelector(".indicators");
+    const slides = document.querySelectorAll(".spanSlider");
+    const indicatorFinal = slides.length / 2;
+    let indicatorNow = 0;
+    let next = 0;
 
     if (scrollContainer && prevBtn && nextBtn) {
         const containerWidth = scrollContainer.offsetWidth;
@@ -109,6 +41,39 @@ export function initSlider() {
             }
         };
 
+        // Generar indicadores
+        const createIndicators = () => {
+            for (let i = 0; i < indicatorFinal; i++) {
+                const indicator = document.createElement("div");
+                indicator.classList.add("indicator");
+                indicator.classList.add(`id-indicator-${i}`);
+                if (i === 0) indicator.classList.add("active");
+                indicatorsContainer.appendChild(indicator);
+            }
+        };
+
+        const pintIndicator = (next, prev) => {
+            console.log(next, prev);
+            const indicatorOld = document.querySelector(`.id-indicator-${prev}`);
+            const indicatorNew = document.querySelector(`.id-indicator-${next}`);
+            indicatorNew.classList.add("active");
+            indicatorOld.classList.remove("active");
+        }
+
+        // Actualizar indicadores
+        const updateIndicators = (idIndicator) => {
+            const prevIndicator = indicatorNow;
+            indicatorNow += idIndicator;
+
+            if (indicatorNow >= indicatorFinal) {
+                indicatorNow = 0; // Reiniciar al primer indicador
+            } else if (indicatorNow < 0) {
+                indicatorNow = indicatorFinal - 1; // Ir al último indicador
+            }
+
+            pintIndicator(indicatorNow, prevIndicator);
+        };
+
         /**
          * Evento de desplazamiento con la rueda del mouse.
          * @param {Event} event - El evento de desplazamiento.
@@ -125,6 +90,7 @@ export function initSlider() {
         const handleNextClick = () => {
             scrollContainer.scrollBy({ left: (containerWidth / 2) - 1, behavior: 'smooth' }); // Desplaza el contenido a la derecha suavemente
             setTimeout(checkPosition, 300); // Chequea y ajusta la posición después de un tiempo
+            updateIndicators(1)
         };
 
         /**
@@ -141,11 +107,15 @@ export function initSlider() {
         // Inicializar la posición del scroll
         initializePosition();
 
+        // Crear indicadores
+        createIndicators();
+
         // Asignar los eventos correspondientes
-        scrollContainer.addEventListener("wheel", handleWheelScroll);
+        // scrollContainer.addEventListener("wheel", handleWheelScroll);
         nextBtn.addEventListener("click", handleNextClick);
         prevBtn.addEventListener("click", handlePrevClick);
     } else {
         console.error("No se encontraron los elementos del slider."); // Muestra un error si los elementos no fueron encontrados
     }
 }
+
